3eb008bde6fea2282ca3be871a5639f7da378aa4
mateusdemirandapereira <terrymafa@gmail.com>
2025-03-17 20:49:46 -0300

    implementando as comparações das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -25,7 +25,7 @@ int main() {
 	scanf("%s", nomeDaCidade);
 
 	printf("Digite a quantidade de habitantes\n");
-	scanf("%d", &populacao);
+	scanf("%ld", &populacao);
 
 	printf("Digite a area da Cidade\n");
 	scanf("%f", &area);
@@ -66,7 +66,7 @@ int main() {
 	scanf("%s", nomeDaCidade2);
 
 	printf("Digite a quantidade de habitantes\n");
-	scanf("%d", &populacao2);
+	scanf("%ld", &populacao2);
 
 	printf("Digite a area da Cidade\n");
 	scanf("%f", &area2);
@@ -88,7 +88,7 @@ int main() {
 	printf("Estado: %c\n", estado);
 	printf("Codigo %s\n", codigoDaCarta);
 	printf("Nome da Cidade: %s\n", nomeDaCidade);
-	printf("Populacao: %d\n", populacao);
+	printf("Populacao: %ld\n", populacao);
 
 	printf("Area: %.2f Km²\n", area);
 	printf("PIB: %.2f bilhoes de reais\n", pib);
@@ -96,19 +96,42 @@ int main() {
 	printf("Densidade Populaciona: %.2f hab/km²\n", densidadePopulacional);
 	printf("PIB per Capita: %.2f reais\n",pibPercapita);
 
+
+
 	printf("\n");
 
 	printf("Carta 2:\n");
 	printf("Estado: %c\n", estado2);
 	printf("Codigo %s\n", codigoDaCarta2);
 	printf("Nome da Cidade: %s\n", nomeDaCidade2);
-	printf("Populacao: %d\n", populacao2);
+	printf("Populacao: %ld\n", populacao2);
 
 	printf("Area: %.2f Km²\n", area2);
 	printf("PIB: %.2f bilhoes de reais\n", pib2);
 	printf("Numero de Pontos Turisticos: %d\n", numeroDePontosTuristicos2);
 	printf("Densidade Populaciona: %.2f hab/km²\n", densidadePopulacional2);
 	printf("PIB per Capita: %.2f reais\n",pibPercapita2);
+
+
+
+	printf("\n\n");
+	printf("Comparação de Cartas:\n");
+
+	int carp = populacao > populacao2 ? 1 : 2;
+	int cara = area > area2 ? 1 : 2;
+	int carpib = pib > pib2 ? 1 : 2;
+	int carTuris = numeroDePontosTuristicos > numeroDePontosTuristicos2 ? 1 : 2;
+	int carDen = densidadePopulacional  < densidadePopulacional2 ? 1: 2;
+	int carper = pibPercapita > pibPercapita2 ? 1 : 2;
+	int carpoder = superPoder > superPoder2 ? 1 : 2;
+
+	printf("População: Carta %d venceu (%d)\n", carp, populacao > populacao2);
+	printf("Área: Carta %d venceu (%d)\n", cara, area > area2);
+	printf("PIB: Carta %d venceu (%d)\n", carpib, pib > pib2);
+	printf("Pontos Turisticos: Carta %d venceu (%d)\n", carTuris, numeroDePontosTuristicos > numeroDePontosTuristicos2);
+	printf("Densidade Populacional: Carta %d venceu (%d)\n", carDen, densidadePopulacional  < densidadePopulacional2);
+	printf("PIB per Capita: carta %d venceu (%d)\n", carper, pibPercapita > pibPercapita2);
+	printf("Super Poder: Carta %d venceu (%d)\n",carpoder, superPoder > superPoder2);
 
 
 
